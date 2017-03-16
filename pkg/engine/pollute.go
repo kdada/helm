479039ef98ffbe00365fe095d88ff751795b2b5a
Kira <me@imkira.com>
2017-03-16 18:08:59 +0800

    add namespace/release/revision to resource annotation

--- pkg/engine/pollute.go
+++ pkg/engine/pollute.go
@@ -18,32 +18,23 @@ package engine
 
 import (
 	"bytes"
-	"os"
+	"fmt"
 
-	"k8s.io/kubernetes/pkg/api"
-	"k8s.io/kubernetes/pkg/api/meta"
-	"k8s.io/kubernetes/pkg/api/v1"
-	"k8s.io/kubernetes/pkg/apimachinery/announced"
-	"k8s.io/kubernetes/pkg/apimachinery/registered"
-	_ "k8s.io/kubernetes/pkg/apis/apps/install"
-	_ "k8s.io/kubernetes/pkg/apis/authentication/install"
-	_ "k8s.io/kubernetes/pkg/apis/authorization/install"
-	_ "k8s.io/kubernetes/pkg/apis/autoscaling/install"
-	_ "k8s.io/kubernetes/pkg/apis/batch/install"
-	_ "k8s.io/kubernetes/pkg/apis/certificates/install"
-	_ "k8s.io/kubernetes/pkg/apis/componentconfig/install"
-	_ "k8s.io/kubernetes/pkg/apis/extensions/install"
-	_ "k8s.io/kubernetes/pkg/apis/imagepolicy/install"
-	_ "k8s.io/kubernetes/pkg/apis/policy/install"
-	_ "k8s.io/kubernetes/pkg/apis/rbac/install"
-	_ "k8s.io/kubernetes/pkg/apis/storage/install"
-	"k8s.io/kubernetes/pkg/runtime"
-	"k8s.io/kubernetes/pkg/runtime/serializer/json"
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/runtime/serializer/json"
+	"k8s.io/client-go/kubernetes/scheme"
+	app "k8s.io/client-go/pkg/api/v1"
+	apps "k8s.io/client-go/pkg/apis/apps/v1beta1"
+	batch "k8s.io/client-go/pkg/apis/batch/v1"
+	extensions "k8s.io/client-go/pkg/apis/extensions/v1beta1"
 )
 
 const (
-	// defaultPollutantKey is the key of pollutant
-	defaultPollutantKey = "helm.sh/pollutant"
+	// defaultPathKey is the key of chart path
+	defaultPathKey      = "helm.sh/pollutant"
+	defaultNamespaceKey = "helm.sh/namespace"
+	defaultReleaseKey   = "helm.sh/release"
+	defaultRevisionKey  = "helm.sh/revision"
 )
 
 var (
@@ -53,34 +44,87 @@ var (
 
 func init() {
 	// create default serializer
-	schema := runtime.NewScheme()
-	errs := []error{api.AddToScheme(schema), v1.AddToScheme(schema)}
-	for _, err := range errs {
-		if err != nil {
-			panic(err)
-		}
-	}
-	announced.DefaultGroupFactoryRegistry.RegisterAndEnableAll(registered.NewOrDie(os.Getenv("KUBE_API_VERSIONS")), schema)
-	defaultSerializer = json.NewYAMLSerializer(json.DefaultMetaFactory, schema, schema)
+	defaultSerializer = json.NewYAMLSerializer(json.DefaultMetaFactory, scheme.Scheme, scheme.Scheme)
 }
 
 // pollute adds pollutant to resource annotations. If resource is not a valid kubernetes
-// resource, it does not pollute the resource and returns original resource.
-func pollute(resource string, pollutant string) string {
+// resource, it does nothing and returns original resource.
+func pollute(resource string, r *renderable) string {
+	// decode object
 	obj, _, err := defaultSerializer.Decode([]byte(resource), nil, nil)
 	if err != nil {
 		return resource
 	}
-	err = meta.NewAccessor().SetAnnotations(obj, map[string]string{
-		defaultPollutantKey: pollutant,
-	})
+	accessor := meta.NewAccessor()
+	annotations, err := accessor.Annotations(obj)
 	if err != nil {
 		return resource
 	}
+	err = accessor.SetAnnotations(obj, merge(annotations, r))
+	if err != nil {
+		return resource
+	}
+
+	// check and pollute specific types
+	switch ins := obj.(type) {
+	case *extensions.Deployment:
+		{
+			ins.Spec.Template.Annotations = merge(ins.Spec.Template.Annotations, r)
+		}
+	case *extensions.DaemonSet:
+		{
+			ins.Spec.Template.Annotations = merge(ins.Spec.Template.Annotations, r)
+		}
+	case *extensions.ReplicaSet:
+		{
+			ins.Spec.Template.Annotations = merge(ins.Spec.Template.Annotations, r)
+		}
+	case *apps.StatefulSet:
+		{
+			ins.Spec.Template.Annotations = merge(ins.Spec.Template.Annotations, r)
+		}
+	case *batch.Job:
+		{
+			ins.Spec.Template.Annotations = merge(ins.Spec.Template.Annotations, r)
+		}
+	case *app.ReplicationController:
+		{
+			ins.Spec.Template.Annotations = merge(ins.Spec.Template.Annotations, r)
+		}
+	}
+
+	// encode object
 	buf := bytes.NewBuffer(nil)
 	err = defaultSerializer.Encode(obj, buf)
 	if err != nil {
 		return resource
 	}
 	return buf.String()
+}
+
+// merge merges renderable info into origin.
+func merge(origin map[string]string, r *renderable) map[string]string {
+	if origin == nil {
+		origin = make(map[string]string)
+	}
+	origin[defaultPathKey] = r.path
+	values := r.vals.AsMap()
+	rmap, ok := values["Release"]
+	if !ok {
+		return origin
+	}
+	release, ok := rmap.(map[string]interface{})
+	if !ok {
+		return origin
+	}
+	if value, ok := release["Namespace"]; ok {
+		origin[defaultNamespaceKey] = fmt.Sprint(value)
+	}
+	if value, ok := release["Name"]; ok {
+		origin[defaultReleaseKey] = fmt.Sprint(value)
+	}
+	if value, ok := release["Revision"]; ok {
+		origin[defaultRevisionKey] = fmt.Sprint(value)
+	}
+	return origin
 }
