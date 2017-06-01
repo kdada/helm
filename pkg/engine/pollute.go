20108d123c944bcf42649a8aa4257329a34bd108
Kira <me@imkira.com>
2017-06-01 10:40:19 +0800

    move engine utils to releaseutil

--- pkg/engine/pollute.go
+++ pkg/engine/pollute.go
@@ -17,114 +17,33 @@ limitations under the License.
 package engine
 
 import (
-	"bytes"
 	"fmt"
 
-	"k8s.io/apimachinery/pkg/api/meta"
-	"k8s.io/apimachinery/pkg/runtime/serializer/json"
-	"k8s.io/client-go/kubernetes/scheme"
-	app "k8s.io/client-go/pkg/api/v1"
-	apps "k8s.io/client-go/pkg/apis/apps/v1beta1"
-	batch "k8s.io/client-go/pkg/apis/batch/v1"
-	extensions "k8s.io/client-go/pkg/apis/extensions/v1beta1"
+	"k8s.io/helm/pkg/releaseutil"
 )
 
-const (
-	// defaultPathKey is the key of chart path
-	defaultPathKey      = "helm.sh/pollutant"
-	defaultNamespaceKey = "helm.sh/namespace"
-	defaultReleaseKey   = "helm.sh/release"
-	defaultRevisionKey  = "helm.sh/revision"
-)
-
-var (
-	// defaultSerializer is a codec and used for encoding and decoding kubernetes resources
-	defaultSerializer *json.Serializer = nil
-)
-
-func init() {
-	// create default serializer
-	defaultSerializer = json.NewYAMLSerializer(json.DefaultMetaFactory, scheme.Scheme, scheme.Scheme)
-}
-
-// pollute adds pollutant to resource annotations. If resource is not a valid kubernetes
-// resource, it does nothing and returns original resource.
-func pollute(resource string, r *renderable) string {
-	// decode object
-	obj, _, err := defaultSerializer.Decode([]byte(resource), nil, nil)
-	if err != nil {
-		return resource
-	}
-	accessor := meta.NewAccessor()
-	annotations, err := accessor.Annotations(obj)
-	if err != nil {
-		return resource
-	}
-	err = accessor.SetAnnotations(obj, merge(annotations, r))
-	if err != nil {
-		return resource
-	}
-
-	// check and pollute specific types
-	switch ins := obj.(type) {
-	case *extensions.Deployment:
-		{
-			ins.Spec.Template.Annotations = merge(ins.Spec.Template.Annotations, r)
-		}
-	case *extensions.DaemonSet:
-		{
-			ins.Spec.Template.Annotations = merge(ins.Spec.Template.Annotations, r)
-		}
-	case *extensions.ReplicaSet:
-		{
-			ins.Spec.Template.Annotations = merge(ins.Spec.Template.Annotations, r)
-		}
-	case *apps.StatefulSet:
-		{
-			ins.Spec.Template.Annotations = merge(ins.Spec.Template.Annotations, r)
-		}
-	case *batch.Job:
-		{
-			ins.Spec.Template.Annotations = merge(ins.Spec.Template.Annotations, r)
-		}
-	case *app.ReplicationController:
-		{
-			ins.Spec.Template.Annotations = merge(ins.Spec.Template.Annotations, r)
-		}
-	}
-
-	// encode object
-	buf := bytes.NewBuffer(nil)
-	err = defaultSerializer.Encode(obj, buf)
-	if err != nil {
-		return resource
-	}
-	return buf.String()
-}
-
-// merge merges renderable info into origin.
-func merge(origin map[string]string, r *renderable) map[string]string {
-	if origin == nil {
-		origin = make(map[string]string)
-	}
-	origin[defaultPathKey] = r.path
+// pollute adds pollutant to resource annotations.
+func pollute(resources string, r *renderable) string {
+	annos := make(map[releaseutil.AnnotationKey]string)
 	values := r.vals.AsMap()
-	rmap, ok := values["Release"]
-	if !ok {
-		return origin
+	if rmap, ok := values["Release"]; ok {
+		if release, ok := rmap.(map[string]interface{}); ok {
+			if value, ok := release["Namespace"]; ok {
+				annos[releaseutil.DefaultNamespaceKey] = fmt.Sprint(value)
+			}
+			if value, ok := release["Name"]; ok {
+				annos[releaseutil.DefaultReleaseKey] = fmt.Sprint(value)
+			}
+		}
 	}
-	release, ok := rmap.(map[string]interface{})
-	if !ok {
-		return origin
+	annos[releaseutil.DefaultPathKey] = r.path
+	docs := releaseutil.SplitManifests(resources)
+	result := ""
+	for _, doc := range docs {
+		if result != "" {
+			result += "\n---\n"
+		}
+		result += releaseutil.InjectAnnotations(doc, annos)
 	}
-	if value, ok := release["Namespace"]; ok {
-		origin[defaultNamespaceKey] = fmt.Sprint(value)
-	}
-	if value, ok := release["Name"]; ok {
-		origin[defaultReleaseKey] = fmt.Sprint(value)
-	}
-	if value, ok := release["Revision"]; ok {
-		origin[defaultRevisionKey] = fmt.Sprint(value)
-	}
-	return origin
+	return result
 }
